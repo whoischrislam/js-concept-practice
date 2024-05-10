d4b211466072b2ee3fe48483be7536cae9ad8e6a
whoischrislam <whoischrislam@gmail.com>
2024-05-09 14:44:32 -1000

    initial load complete

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // import data
 import { menuArray as menuData } from "./data.js";
 
+// Order
+const currentOrder = [];
+
 // references to existing DOM elements
 const main = document.querySelector("main");
+const emptyContent = document.getElementById("empty-content")
 
 // DOM elements
 // Main div
@@ -13,25 +17,34 @@ menuContainer.id = "menu";
 main.append(menuContainer);
 
 // take data and render them
-const currentMenu = menuData.map( menuItem => {
-    const {name, ingredients, id, price, emoji} = menuItem
-    return `
-        <div class="menu-item">
-            <div class="menu-item-container">
-                <div class="menu-item-image">
-                    ${emoji}
-                </div>
-                <div class="menu-item-info">
-                    <div class="menu-item-name">${name}</div>
-                    <div class="menu-item-ingredients">${ingredients}</div>
-                    <div class="menu-item-price">$${price}</div>
-                </div>
-            </div>
-            <button class="menu-item-btn">+</button>
-        </div>
-        <hr />
-    `;
-}).join("");
+if (menuData.length !== 0) {
+    emptyContent.style.display = "none";
+    renderMenu(menuData);
+} else {
+    console.log("no data");
+}
 
-menuContainer.innerHTML = currentMenu;
-console.log(currentMenu);
+// render menu from an array of objects
+function renderMenu(menuData) {
+    const currentMenu = menuData.map( menuItem => {
+        const {name, ingredients, id, price, emoji} = menuItem
+        return `
+            <div class="menu-item">
+                <div class="menu-item-container">
+                    <div class="menu-item-image">
+                        ${emoji}
+                    </div>
+                    <div class="menu-item-info">
+                        <div class="menu-item-name">${name}</div>
+                        <div class="menu-item-ingredients">${ingredients}</div>
+                        <div class="menu-item-price">$${price}</div>
+                    </div>
+                </div>
+                <button class="menu-item-btn">+</button>
+            </div>
+            <hr />
+        `;
+    }).join("");
+    
+    menuContainer.innerHTML = currentMenu;
+}
