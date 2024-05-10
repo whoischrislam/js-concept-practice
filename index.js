7f48bde150984ff4cd0cfdbd301f91fd507dc5fc
whoischrislam <whoischrislam@gmail.com>
2024-05-09 14:28:44 -1000

    style update and js data render

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 // import data
-import { menuArray as menu, menuArray } from "./data.js";
+import { menuArray as menuData } from "./data.js";
 
+// references to existing DOM elements
+const main = document.querySelector("main");
+
+// DOM elements
+// Main div
+const menuContainer = document.createElement("div");
+menuContainer.id = "menu";
+
+// appending elements
+main.append(menuContainer);
+
+// take data and render them
+const currentMenu = menuData.map( menuItem => {
+    const {name, ingredients, id, price, emoji} = menuItem
+    return `
+        <div class="menu-item">
+            <div class="menu-item-container">
+                <div class="menu-item-image">
+                    ${emoji}
+                </div>
+                <div class="menu-item-info">
+                    <div class="menu-item-name">${name}</div>
+                    <div class="menu-item-ingredients">${ingredients}</div>
+                    <div class="menu-item-price">$${price}</div>
+                </div>
+            </div>
+            <button class="menu-item-btn">+</button>
+        </div>
+        <hr />
+    `;
+}).join("");
+
+menuContainer.innerHTML = currentMenu;
+console.log(currentMenu);
